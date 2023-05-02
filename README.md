c76156750cd7c08f37ec0938a0d87392a76a03fc
d <d.mirauta@protonmail.com>
2023-05-02 20:58:54 +0100

    autogen

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
  
 Basic set of configs (mostly from corresponding defaults) for a bspwm based desktop environment.
 
-Minimum changes:
-- `config.ini`: define `monitor = ` (per bar) for multi monitor setup or comment out otherwise.
-- `bspwmrc`: Edit wallpaper paths(s) for `feh`, number of polybars and `bspc monitor` lines.
-
-Use `auto.sh` to install dependencies and insert modified configs in the relevant directories.
+For a minimum change quickstart, edit the quickstart section in `auto.sh` to suit your monitor setup and use `./auto.sh generate` to generate configs. These can be installed with `./auto.sh update`. Dependencies can be grabbed with the `./auto.sh deps` (and optionally `./auto.sh lapdeps` for laptop only components) command.
 
 Todo:
-- Autogen multimonitor configs (minimise min changes and put in one place)
 - option to quickly swap arrows and hjkl in keybinds for whichever is prefered as promminent?
 
 Notes:
+- some configs do not like comments after definitions! (keep comments on separate lines)
 - spectacle is nice but may bring in too many KDE dependencies? (Otherwise KDE system settings is also quite nice)
 - many use thunar as part of their minimal setup so xfce4 apps are not a worry? (hence xfce4-power-manager is ok)
     - otherwise ranger and/or broot
+
--- auto.sh
+++ auto.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 
+#### Quickstart
+
+WALLBASE=$HOME/Pictures/dual_1440p/cuts
+export WALLPAPERS="$WALLBASE/6593144_right.jpg $WALLBASE/6593144_left.jpg"
+# order acording to `xrandr --listmonitors`
+DISPLAYS=("DisplayPort-2" "DisplayPort-1") # can leave empty () for single monitor
+WORKSPACES=("I II III IV" "V VI VII VIII")
+N_MONITORS=2
+
+#### 
+
+INSTALL="pacman -S"
+
 DOTDIRS=( .xinitrc:$HOME
            bspwmrc:$HOME/.config/bspwm/ 
            sxhkdrc:$HOME/.config/sxhkd/
            dunstrc:$HOME/.config/dunst/
            config.ini:$HOME/.config/polybar/ )
-
-INSTALL="pacman -S"
+ 
 
 case $1 in
+
+  generate)
+
+    NL=$'\n'
+    BARDEFS=""
+    BARSENABLE=""
+    BSPCMON=""
+    let I=N_MONITORS-1
+    for i in $(seq 0 $I) ; do
+      export BARID="BAR$i"
+      export MON="monitor = ${DISPLAYS[i]}"
+      export BAR="$(envsubst < templates/poly.bar)"
+      export BARDEFS="${BARDEFS}${BAR}${NL}"
+      export BARSENABLE="${BARSENABLE}polybar $BARID &$NL"
+      export BSPCMON="${BSPCMON}bspc monitor ${DISPLAYS[i]} -d ${WORKSPACES[i]} &$NL"
+    done
+    envsubst < templates/config.ini > config.ini
+    envsubst < templates/bspwmrc > bspwmrc
+
+    ;;
 
   deps)
     echo "Installing main packages..."
@@ -33,7 +65,7 @@ case $1 in
     ;;
 
   *)
-    echo Issue \'deps\', \'lapdeps\' or \'update\' as argument.
+    echo Issue \'generate\', \'deps\', \'lapdeps\' or \'update\' as argument.
     ;;
 
 esac
