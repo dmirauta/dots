4870d8976cd664cdd5d800be6cc0f41f44a61879
d <d.mirauta@protonmail.com>
2023-05-07 17:57:02 +0100

    tidy up

--- scripts/cheat_sheet.sh
+++ scripts/cheat_sheet.sh
@@ -1,3 +1,3 @@
 
-grep -Pzo "\\n# \S.*\\n.*\\n" $HOME/.config/sxhkd/sxhkdrc | tr '\n' ':' | sed 's/:#/\n/g' | sed 's/:/   ##   /g' | rofi -dmenu -theme-str 'window {width: 80%;}' -theme-str 'listview {columns:1;}'
+grep -Pzo "\\n# \S.*\\n.*\\n" $HOME/.config/sxhkd/sxhkdrc | tr '\n' ':' | sed 's/:#/\n/g' | sed 's/:/   ##   /g' | rofi -dmenu -p "Cheat sheet (input map)" -theme-str 'window {width: 80%;}' -theme-str 'listview {columns:1;}'
 
