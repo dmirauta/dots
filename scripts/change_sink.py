c202787b43f9d56b1650527bbf28ce6f36682f55
d <d.mirauta@protonmail.com>
2023-06-04 19:59:07 +0100

    non-sequential sinks

---
+++ scripts/change_sink.py
@@ -0,0 +1,18 @@
+import re
+from subprocess import getoutput
+
+SINKS = getoutput("pactl list sinks")
+PATTERN = r"Sink #(\d+)(?:\n|.*?)*Description: ([a-zA-Z0-9 ]+)"
+
+SINK_NUMS, SINK_NAMES = zip(*re.findall(PATTERN, SINKS, re.MULTILINE))
+SINK_DICT = dict(zip(SINK_NAMES, SINK_NUMS))
+
+OPTIONS = "\n".join(SINK_NAMES)
+SELECTED = getoutput(f"echo -e \"{OPTIONS}\" | rofi -dmenu -p \"Select audio out\"")
+if SELECTED=='': exit()
+print(f"selected {SELECTED}")
+
+SINK_INPUTS = getoutput("pacmd list-sink-inputs | grep index").split(": ")[1].strip()
+for si in SINK_INPUTS.split("\n"):
+    print(f"Changing input {si} to sink {SELECTED}")
+    print(getoutput(f"pacmd move-sink-input {si} {SINK_DICT[SELECTED]}"))
