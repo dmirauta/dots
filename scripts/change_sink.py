653f94d9f55d447c41736746d648a40b29fb2b19
d <d.mirauta@protonmail.com>
2023-06-04 21:50:02 +0100

    fix sink select

--- scripts/change_sink.py
+++ scripts/change_sink.py
@@ -12,7 +12,8 @@ SELECTED = getoutput(f"echo -e \"{OPTIONS}\" | rofi -dmenu -p \"Select audio out
 if SELECTED=='': exit()
 print(f"selected {SELECTED}")
 
-SINK_INPUTS = getoutput("pacmd list-sink-inputs | grep index").split(": ")[1].strip()
-for si in SINK_INPUTS.split("\n"):
+SINK_INPUTS = getoutput("pacmd list-sink-inputs | grep index").strip()
+for line in SINK_INPUTS.split("\n"):
+    si = line.split(": ")[1]
     print(f"Changing input {si} to sink {SELECTED}")
     print(getoutput(f"pacmd move-sink-input {si} {SINK_DICT[SELECTED]}"))
