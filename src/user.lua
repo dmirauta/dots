1aaace9079ef3e3a33ae917d62bdcde0c60715dc
d <d.mirauta@protonmail.com>
2023-08-30 18:08:22 +0100

    remove unused package

--- src/user.lua
+++ src/user.lua
@@ -2,9 +2,9 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "ubaldot/vim-conda-activate",
-  {
-	"kmontocam/nvim-conda",
-	dependencies = { "nvim-lua/plenary.nvim" },
-  ft = "python",
-  },
+ --  {
+	-- "kmontocam/nvim-conda",
+	-- dependencies = { "nvim-lua/plenary.nvim" },
+ --  ft = "python",
+ --  },
 }
