c774af616ef581b5d78f770206cc6c9ca25081fe
d <d.mirauta@protonmail.com>
2023-11-16 16:02:53 +0000

    more terminal friendly binding

--- src/community.lua
+++ src/community.lua
@@ -2,8 +2,11 @@ return {
   "AstroNvim/astrocommunity",
    { import = "astrocommunity.pack.rust" },
    { import = "astrocommunity.pack.python" },
-   { import = "astrocommunity.pack.markdown" },
    { import = "astrocommunity.pack.bash" },
    { import = "astrocommunity.pack.cpp" },
    { import = "astrocommunity.markdown-and-latex.vimtex" },
+   -- { import = "astrocommunity.pack.markdown" },
+   -- { import = "astrocommunity.pack.godot" },
+   -- { import = "astrocommunity.pack.haskell" },
+   -- { import = "astrocommunity.pack.docker" },
 }
--- src/user.lua
+++ src/user.lua
@@ -1,10 +1,12 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "ubaldot/vim-conda-activate",
- --  {
-	-- "kmontocam/nvim-conda",
-	-- dependencies = { "nvim-lua/plenary.nvim" },
- --  ft = "python",
- --  },
+  -- {
+  --    "nvim-telescope/telescope.nvim",
+  --     defaults = {
+  --         vimgrep_arguments = {
+  --           "rg",
+  --          }
+  --     }
+  -- }
 }
